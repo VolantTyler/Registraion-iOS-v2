7fbf2988dae47e89f2c3d835a250a3dd4715f3c6
Tyler Stahl <tylerstahl@gmail.com>
2024-09-04 11:01:41 -0400

    add button functionality

--- Registraion v2/ViewController.swift
+++ Registraion v2/ViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("Register")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("This is my first piece of Swift code.")
+        
+        label.text=("Please register your visit using this wonderful app")
     }
 
 
